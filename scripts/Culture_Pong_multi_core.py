502d6467e0154b0a3dd2de4f6456001ef17a608e
juliuswue <julius@wuerzler.de>
2026-01-02 11:22:57 +0100

    subtract correct sum with degree

--- scripts/Culture_Pong_multi_core.py
+++ scripts/Culture_Pong_multi_core.py
@@ -37,9 +37,11 @@ eqs_syn = '''
 dw = r_pre/Hz * r_post/Hz * (r_post - theta) : Hz (constant over dt)
 ddelta_w/dt = 1 / Tau_W * (dw - delta_w) : Hz (clock-driven)
 dw/dt = Lr * (
-    delta_w * (int(delta_w < 0*Hz) * int(w > 0.01) * A_ltd + int(delta_w > 0*Hz) * int(w < 0.5) * int(sum_w_pre < W_sum_max))
+    delta_w * (int(delta_w < 0*Hz) * int(w > 0.01) * A_ltd + int(delta_w > 0*Hz) * int(w < 0.5))
+    )
+    - 1 / DEG * (sum_w_pre - W_sum_max) * int(sum_w_pre > W_sum_max) * int(w > 0.01) * Hz 
     #- C * int(sum_w_pre > W_sum_max) * int(w > 0.01) * (sum_w_pre - W_sum_max) * Hz
-) : 1 (clock-driven)
+: 1 (clock-driven)
 theta = (r_slow_post)**2 / R_0 : Hz (constant over dt)
 u_syn_post = w * r_pre / Hz : 1 (summed)
 sum_w_pre = w : 1 (summed)
@@ -49,6 +51,7 @@ A_ltd : 1 (constant)
 R_0 : Hz (constant)
 C : 1 (constant)
 Tau_W : second (constant)
+DEG : 1 (constant)
 '''
 
 # ----------------------- Parameters -----------------------
@@ -78,7 +81,7 @@ N_RUNS = 200
 T_INIT = 300
 N_PARAM_SETS = 1_000
 N_CPU_CORES = 80
-N_NETWORKS_PER_PARAM_SET = 5
+N_NETWORKS_PER_PARAM_SET = 20
 
 # ----------------------- Functions -----------------------
 # --- plot ---
@@ -275,6 +278,11 @@ def create_network(args):
         ids = isin(synapses.j, j)
         synapses.w[:][ids] = synapses.w[:][ids] / sum(synapses.w[:][ids]) * 0.5
         
+    for pre in range(n_neurons):
+        ids = isin(synapses.i, pre)
+        synapses.DEG[:][ids] = sum(ids)
+        synapses.w[:][ids] = synapses.w[:][ids] / sum(synapses.w[:][ids]) * 0.5
+        
     # set synapse paramters
     synapses.Lr = args["Lr"]
     synapses.W_sum_max = args["W_sum_max"]
@@ -382,9 +390,9 @@ def run_one_paramter_set(trial):
     p_eff = 0.5 #trial.suggest_float("eff", 0.3, 0.7)
     p_readout_acc =  trial.suggest_float("readout_acc", 1e-3, 2e-1)
     p_W_sum = trial.suggest_float("W_sum", 0.7, 2.5)
-    p_A_ltd = 1# trial.suggest_float("A_ltd", 0.5, 5)
+    p_A_ltd = trial.suggest_float("A_ltd", 0.5, 5)
     p_C = 0#trial.suggest_float("C", 0, 20)
-    p_Lr = 5e-5#  trial.suggest_float("Lr", 1e-4, 1e-5)
+    p_Lr = trial.suggest_float("Lr", 1e-5, 1e-4)
     p_tau = 10#trial.suggest_float("Tau", 75, 100)
     p_tau_W = 100
     
@@ -392,7 +400,7 @@ def run_one_paramter_set(trial):
     os.makedirs(outdir, exist_ok=True)
     
     
-    for random_seed in [0, 2, 3, 4, 1]:
+    for random_seed in range(N_NETWORKS_PER_PARAM_SET):
         args_list.append(
             {
                 "random_seed": random_seed,
@@ -435,7 +443,7 @@ def run_optimization(_):
         direction='maximize',
         pruner=optuna.pruners.MedianPruner(
             n_startup_trials=N_CPU_CORES, 
-            n_warmup_steps=2,
+            n_warmup_steps=5,
             interval_steps=1
         )
     )
